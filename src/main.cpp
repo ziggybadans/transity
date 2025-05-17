6be6b4b7cd218de52d40c68f266938929bddd4f7
ziggybadans <ziggyspike@hotmail.com.au>
2025-05-17 15:22:26 +1000

    Fixed

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,21 @@
-int main() {
-    return 0;
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
